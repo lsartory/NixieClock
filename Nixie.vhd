6eb8d1702db31a8aa100ff696952b40775a80daa
lsartory <lambert.sartory@gmail.com>
2018-03-29 11:52:27 +0200

    Initial commit.

---
+++ Nixie.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Nixie is
+	port
+	(
+		CLK : in std_logic
+	);
+end Nixie;
+
+
+architecture Nixie_Arch of Nixie is
+begin
+
+end architecture;
